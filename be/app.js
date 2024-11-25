da65a4d98c5e36832346db5c8f9de4e9fd751b6c
Gigliárly Marcelino <gigliarlymarcelino@gmail.com>
2024-11-25 15:11:55 -0300

    Update app.js

--- be/app.js
+++ be/app.js
@@ -7,7 +7,10 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-app.use('/usuarios', userRoutes);
+app.use('/users', userRoutes);
+app.get('/', () => {
+    res.send("Welcome to Evolux BE")
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
